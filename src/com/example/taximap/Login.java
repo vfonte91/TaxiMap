c8ccc92306f8105cef71ffd5fb0a7aade2fb20bb
Vince Fonte <v.fonte91@gmail.com>
2013-04-02 15:14:03 -0400

    Added user credentials persistence

--- src/com/example/taximap/Login.java
+++ src/com/example/taximap/Login.java
@@ -3,6 +3,8 @@ package com.example.taximap;
 import com.example.taximap.R;
 import com.example.taximap.db.QueryDatabaseLogin;
 
+import android.accounts.AccountManager;
+import android.accounts.Account;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -15,26 +17,32 @@ import android.widget.EditText;
 public class Login extends Activity implements OnClickListener{
    private EditText userNameEditableField;
    private EditText passwordEditableField;
+   private AccountManager mAccountManager;
 
    private static final String TAG = "Login Activity";
     @Override
     public void onCreate(Bundle savedInstanceState) {
-    	super.onCreate(savedInstanceState);		
-    	setContentView(R.layout.login);
-    	//initialize instance variables
-        userNameEditableField=(EditText)findViewById(R.id.username_text);
-        passwordEditableField=(EditText)findViewById(R.id.password_text);
-
-        View btnLogin=(Button)findViewById(R.id.login_button);
-        btnLogin.setOnClickListener(this);		//Defined in the onclick function below
-
-        /*  Removed because home button will take care of cancel
-        View btnCancel=(Button)findViewById(R.id.cancel_button);
-        btnCancel.setOnClickListener(this);
-        */
-
-        View btnNewUser=(Button)findViewById(R.id.new_user_button);
-        btnNewUser.setOnClickListener(this);
+    	super.onCreate(savedInstanceState);
+        mAccountManager = AccountManager.get(this);
+        
+        //Check if account exists
+        if (!(savedAccount())){
+	    	setContentView(R.layout.login);
+	    	//initialize instance variables
+	        userNameEditableField=(EditText)findViewById(R.id.username_text);
+	        passwordEditableField=(EditText)findViewById(R.id.password_text);
+	
+	        View btnLogin=(Button)findViewById(R.id.login_button);
+	        btnLogin.setOnClickListener(this);		//Defined in the onclick function below
+	
+	        /*  Removed because home button will take care of cancel
+	        View btnCancel=(Button)findViewById(R.id.cancel_button);
+	        btnCancel.setOnClickListener(this);
+	        */
+	
+	        View btnNewUser=(Button)findViewById(R.id.new_user_button);
+	        btnNewUser.setOnClickListener(this);
+        }
      }
 
     // The commented functions are for due 3
@@ -68,16 +76,29 @@ public class Login extends Activity implements OnClickListener{
     	Log.i(TAG, "onRestart()");
     }*/
     
-    private void checkLogin(){
-    	String username=this.userNameEditableField.getText().toString();
-        String password=this.passwordEditableField.getText().toString();
+    private boolean savedAccount() {
+    	boolean result = false;
+    	String username, password;
+    	Account[] accounts = mAccountManager.getAccountsByType(Constants.ACCOUNT_TYPE);
+    	if (accounts.length > 0) {
+    		//There maybe more than one account, so the last one created is used
+    		Account userAccount = accounts[accounts.length - 1];
+    		username = mAccountManager.getUserData(userAccount, Constants.USER_DATA_KEY);
+        	password = mAccountManager.getPassword(userAccount);
+        	checkLogin(username, password);
+        	result = true;
+    	}
+		return result;
+    }
+    
+    private void checkLogin(String username, String password){
         new QueryDatabaseLogin(this).execute(username, password);
       }
 
     public void onClick(View v) {			//Login activity's click event
 		switch (v.getId()) {
   		case R.id.login_button:
-		    checkLogin();
+		    checkLogin(this.userNameEditableField.getText().toString(), this.passwordEditableField.getText().toString());
 		    break;
 		    /* Removed because home button will take care of cancel
   		case R.id.cancel_button:
@@ -85,7 +106,7 @@ public class Login extends Activity implements OnClickListener{
     		break;
 		     */
     	case R.id.new_user_button:
-    	    startActivity(new Intent(this, Account.class));		// start a new activity Direct invocation 
+    	    startActivity(new Intent(this, AccountActivity.class));		// start a new activity Direct invocation 
     	    break;
 		}
     }
--- src/com/example/taximap/db/QueryDatabaseNewUser.java
+++ src/com/example/taximap/db/QueryDatabaseNewUser.java
@@ -14,31 +14,41 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 
-import com.example.taximap.Account;
+import com.example.taximap.AccountActivity;
+import com.example.taximap.Constants;
 
+import android.accounts.AccountManager;
+import android.accounts.Account;
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.os.AsyncTask;
+import android.os.Bundle;
 import android.util.Log;
 
 
 public class QueryDatabaseNewUser  extends AsyncTask<String, Void, Integer>{
 	int puid=-1;
 	Context context;
+	private AccountManager mAccountManager;
+	private String username;
+	private String password;
 	
 	public QueryDatabaseNewUser(Context context) {
         this.context = context;
+        mAccountManager = AccountManager.get(context);
     }
 	
-	protected Integer doInBackground(String... username) {
+	protected Integer doInBackground(String... credentials) {
 		Integer status=0;
 		String line;
 		//the data to send
 		ArrayList<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
-		nameValuePairs.add(new BasicNameValuePair("username",username[0]));
-		nameValuePairs.add(new BasicNameValuePair("password",username[1]));
+		username = credentials[0];
+		password = hashString(credentials[1]);
+		nameValuePairs.add(new BasicNameValuePair("username",username));
+		nameValuePairs.add(new BasicNameValuePair("password",password));
 		 
 		//http post
 		try{
@@ -59,7 +69,15 @@ public class QueryDatabaseNewUser  extends AsyncTask<String, Void, Integer>{
 	}
 	
 	protected void onPostExecute(Integer result) {
-		if(result==1){           
+		if(result==1){
+			//Create new account based on username and account type
+			Account account = new Account(username, Constants.ACCOUNT_TYPE);
+			//Bundle used to save user data. username is saved
+			Bundle userData = new Bundle();
+			userData.putString(Constants.USER_DATA_KEY, username);
+			//Add account to AccountManager
+			mAccountManager.addAccountExplicitly(account, password, userData);
+			
             new AlertDialog.Builder(context)
     		.setTitle("Message")
     		.setMessage("New User Created!")
@@ -85,5 +103,10 @@ public class QueryDatabaseNewUser  extends AsyncTask<String, Void, Integer>{
     		.show();
         }
     }
+	
+	private String hashString(String string) {
+		int hash  = string.hashCode();
+		return Integer.toString(hash);
+	}
 
 }
