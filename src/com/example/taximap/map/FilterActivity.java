420849a78d24b14a289f3f007ac9372cf9a43338
Vince Fonte <v.fonte91@gmail.com>
2013-04-09 14:54:00 -0400

    Removed unused stuff

--- src/com/example/taximap/map/FilterActivity.java
+++ src/com/example/taximap/map/FilterActivity.java
@@ -4,7 +4,6 @@ import java.util.HashMap;
 import java.util.Map;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.SparseArray;
 import android.view.View;
